a4da36769d907401f2ebdea70f585e14cb75b521
Jorge <chauxjorge@hotmail.com>
2021-09-16 11:12:40 -0500

    Add api configuration and use in Home component to fetch data from API. NOTE: nothing in screen but check information in console vue dev tools

---
+++ src/api.js
@@ -0,0 +1,20 @@
+// Aqui (api.js) van las diferentes implementaciones de la API de coinCAP
+// https://docs.coincap.io/?version=latest
+
+const url = "https://api.coincap.io/v2";
+
+// funciones que usaran fetch para acceder y obtener información de la API
+// usamos promises para obtener respuesta en formato json
+// data es el objeto que viene dentro del body de la response
+// data --- porque en la API de coinCap vemos que dice data
+// https://docs.coincap.io/?version=latest#ee0c0be6-513f-4466-bbb0-2016add462e9
+
+function getAssets() {
+  return fetch(`${url}/assets?limit=20`)
+    .then((resp) => resp.json())
+    .then((res) => res.data);
+}
+
+export default {
+  getAssets,
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,21 @@
 </template>
 
 <script>
+import api from "@/api";
 import PxAssetsTable from "@/components/PxAssetsTable.vue";
 export default {
   name: "Home",
 
   components: { PxAssetsTable },
+
+  data() {
+    return {
+      assets: [],
+    };
+  },
+
+  created() {
+    api.getAssets().then((assetsReceived) => (this.assets = assetsReceived));
+  },
 };
 </script>
