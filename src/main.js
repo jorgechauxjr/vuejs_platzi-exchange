fd232260a0f08131aa8826104e47740f7d9303b5
Jorge <chauxjorge@hotmail.com>
2021-09-16 16:31:08 -0500

    Import the dollar filter created in filters.js in main.js and use it. Vue.filter(dollar, dolarFilter)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 import router from "@/router";
+import { dollarFilter } from "@/filters";
 
+// Para que funcione el filtro uso .filter()
+// le paso nombre del filtro y la funcion que se va a ejecutar cuando llame ese filtro
+Vue.filter("dollar", dollarFilter);
 Vue.config.productionTip = false;
 
 new Vue({
