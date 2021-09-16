a6a0e8ef712e85520e226eb832def6777729e340
Jorge <chauxjorge@hotmail.com>
2021-09-16 09:54:47 -0500

    Fix router-view name in app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <px-header />
-    <touter-view class="container px-5 sm:px-20 py-20 flex justify-center" />
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
 <script>
