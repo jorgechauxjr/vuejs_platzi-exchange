f9122ad25348eaa7530ac6e5a33697cd09536fc0
Jorge <chauxjorge@hotmail.com>
2021-09-17 09:42:49 -0500

    [fix] Add initial file to start CoinDetail.vue pagina DINAMICA

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="flex-col">
-    <template v-if="asset.id">
+    <template>
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
-          <img
-            :src="
-              `https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`
-            "
-            :alt="asset.name"
-            class="w-20 h-20 mr-5"
-          />
+          <img class="w-20 h-20 mr-5" />
           <h1 class="text-5xl">
-            {{ asset.name }}
-            <small class="sm:mr-2 text-gray-500">{{ asset.symbol }}</small>
+            <small class="sm:mr-2 text-gray-500"></small>
           </h1>
         </div>
 
@@ -20,27 +13,27 @@
           <ul>
             <li class="flex justify-between">
               <b class="text-gray-600 mr-10 uppercase">Ranking</b>
-              <span>#{{ asset.rank }}</span>
+              <span></span>
             </li>
             <li class="flex justify-between">
               <b class="text-gray-600 mr-10 uppercase">Precio actual</b>
-              <span>{{ asset.priceUsd | dollar }}</span>
+              <span></span>
             </li>
             <li class="flex justify-between">
               <b class="text-gray-600 mr-10 uppercase">Precio más bajo</b>
-              <span>{{ min | dollar }}</span>
+              <span></span>
             </li>
             <li class="flex justify-between">
               <b class="text-gray-600 mr-10 uppercase">Precio más alto</b>
-              <span>{{ max | dollar }}</span>
+              <span></span>
             </li>
             <li class="flex justify-between">
               <b class="text-gray-600 mr-10 uppercase">Precio Promedio</b>
-              <span>{{ avg | dollar }}</span>
+              <span></span>
             </li>
             <li class="flex justify-between">
               <b class="text-gray-600 mr-10 uppercase">Variación 24hs</b>
-              <span>{{ asset.changePercent24Hr | percent }}</span>
+              <span></span>
             </li>
           </ul>
         </div>
@@ -48,7 +41,9 @@
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
             class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          >Cambiar</button>
+          >
+            Cambiar
+          </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
@@ -66,63 +61,3 @@
     </template>
   </div>
 </template>
-
-<script>
-import api from '@/api'
-
-export default {
-  name: 'CoinDetail',
-
-  data() {
-    return {
-      asset: {},
-      history: []
-    }
-  },
-
-  computed: {
-    min() {
-      return Math.min(
-        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
-      )
-    },
-
-    max() {
-      return Math.max(
-        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
-      )
-    },
-
-    avg() {
-      return Math.abs(
-        ...this.history.map(h => parseFloat(h.priceUsd).toFixed(2))
-      )
-    }
-  },
-
-  created() {
-    this.getCoin()
-  },
-
-  methods: {
-    getCoin() {
-      const id = this.$route.params.id
-
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(
-        ([asset, history]) => {
-          this.asset = asset
-          this.history = history
-        }
-      )
-    }
-  }
-}
-</script>
-
-<style scoped>
-td {
-  padding: 10px;
-  text-align: center;
-}
-</style>
-
