cd4aaf012c047e0d6fbf03084b42264f6f7fe626
Jorge <chauxjorge@hotmail.com>
2021-09-20 18:06:03 -0500

    Import chart, chartkick and vuespinners in src/main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "@/assets/css/tailwind.css";
+import Chart from "chart.js";
+import Chartick from "vue-chartkick";
+import { VueSpinners } from "@saeris/vue-spinners";
+
 import router from "@/router";
 import { dollarFilter, percentFilter } from "@/filters";
+
+Vue.use(VueSpinners);
+Vue.use(Chartick.use(Chart));
 
 // Para que funcione el filtro uso .filter()
 // le paso nombre del filtro y la funcion que se va a ejecutar cuando llame ese filtro
